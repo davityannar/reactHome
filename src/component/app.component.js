d179c77b433cfadd1878d44036338fbdf6dc25c9
narek <narek_abrahamyan@epam.com>
2019-03-21 17:43:03 +0400

    2HW

---
+++ src/component/app.component.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import s from "./app.component.css";
+class MyComponent extends Component {
+  render() {
+    return <div className={s.intro}>Hello Worldoooooooooooo</div>;
+  }
+}
+export default MyComponent;
